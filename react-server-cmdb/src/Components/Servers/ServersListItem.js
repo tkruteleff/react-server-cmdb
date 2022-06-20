9e053c86e358b779cbea07aab1d8020e2c1b4af5
Teemu Kruteleff <tkruteleff@gmail.com>
2022-06-20 20:07:46 +0300

    Serverslist component added

--- react-server-cmdb/src/Components/Servers/ServersListItem.js
+++ react-server-cmdb/src/Components/Servers/ServersListItem.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const MOC_DATA = {
-    ipAddress: '192.168.1.1',
-    
-}
+const MOC_DATA = [
+    {
+        ipAddress: '192.168.1.1',
+        computerName: 'testmachine01',
+        fqdn: 'testmachine01.domain.local',
+        operatingSystem: 'Windows Server 2022',
+        location: 'Azure',
+        organization: 'organization 1',
+        businessOwner: 'Bic McOwner',
+        techincalOwner: 'Tech McOwner',
+        description: 'Some long description for the server'
+    },
+    {
+        ipAddress: '192.168.1.2',
+        computerName: 'testmachine02',
+        fqdn: 'testmachine02.domain.local',
+        operatingSystem: 'Windows Server 2022',
+        location: 'On-Premises',
+        organization: 'organization 2',
+        businessOwner: 'Bic McOwner',
+        techincalOwner: 'Tech McOwner',
+        description: 'Some long description for the server'
+    }
+]
 
 const ServerListItem = props => {
+
+    const serverMapHandler = MOC_DATA.map((server) => (
+        <li key={server.ipAddress}>
+            <p>{server.ipAddress}</p>
+            <p>{server.fqdn}</p>
+            <p>{server.operatingSystem}</p>
+            <p>{server.location}</p>
+            <p>{server.organization}</p>
+            <p>{server.businessOwner}</p>
+            <p>{server.techincalOwner}</p>
+            <p>{server.description}</p>
+        </li>
+    ))
+
     return (
-        <div>
-            <p>Server list items</p>
-        </div>
+        <Fragment>
+            <ul>
+                {serverMapHandler}
+            </ul>
+        </Fragment>
     );
 };
 
